cb8331d5674e6fc8d467c8ef5e1765afe0f1c1d2
gerardo vega <gerardoluisvega@gmail.com>
2024-08-08 10:45:30 -0300

    Trabajo finalizado

--- src/controllers.js
+++ src/controllers.js
@@ -14,35 +14,49 @@ crtl.mostrarTareas = async(req, res)=>{
 
 crtl.crearTarea = async(req, res)=>{
     const conexion = await conectar();
-    const {title,description,isComplete} = req.body
+    const { title,description,isComplete } = req.body
     if(typeof(title) != "string"){
         res.send("El titulo no cumple con los requisitos")
     }else if(typeof(description) != "string"){
         res.send("La descripcion no cumple con los requisitos")
     }else if(typeof(isComplete) != "boolean"){
-        res.send(" Los datos ingresados en la columna isComplete no cumple con los requisitos")
+        res.send(" Los datos ingresados en la columna isComplete no cumple con los requisitos")    
     }else {
         const consulta = await conexion.query(`INSERT INTO TASKS (title, description, isComplete) VALUES('${title}','${description}','${isComplete}');`);
-        res.send("Se creo una nueva tarea exitosamente")
+    res.send("Se creo una nueva tarea exitosamente") 
     }
 }
 crtl.editarTarea = async(req, res)=>{
     const conexion = await conectar();
-    const id = parseInt(req.params.id)
-    const {title,description,isComplete} = req.body
-    const [ consulta ] = await conexion.query(`UPDATE TASKS SET title ='${title}', description = '${description}', isComplete = ${isComplete} WHERE id = ${id}`)
-    res.send("Se actualizaron los datos correctamente")
+    const { title,description,isComplete } = req.body
+    const id = parseInt(req.params.id) 
+    const verificacion = await conexion.query(`SELECT * FROM TASKS WHERE ID = ${id};`)
+    if (verificacion.length == [] || typeof(title) != "string" || typeof(description) != "string" || typeof(isComplete) != "boolean"){
+        res.send("Algunaos de los datos no coinciden")
+    }else{
+        const [ consulta ] = await conexion.query(`UPDATE TASKS SET title ='${title}', description = '${description}', isComplete = ${isComplete} WHERE id = ${id};`)
+        res.send("Se actualizaron los datos correctamente")
+    }
 }
+
 crtl.buscarTarea = async(req, res)=>{
     const conexion = await conectar();
     const id = parseInt(req.params.id)
-    const [ consulta ] = await conexion.query(`SELECT id, title, description, isComplete FROM TASKS where id = ${id}`)
+    const {verificacion} = await conexion.query(`SELECT * FROM TASKS WHERE ID = ${parseInt(id)};`)
+    if(verificacion.length == []){
+        res.send("la id ingresada no existe")
+    }else{const [ consulta ] = await conexion.query(`SELECT id, title, description, isComplete FROM TASKS where id = ${id}`)
     res.send(consulta)
-}
+        }
+    }
 crtl.eliminarTarea = async(req, res)=>{
     const conexion = await conectar();
     const id = parseInt(req.params.id)
-    const [ consulta ] = await conexion.query(`DELETE FROM TASKS where id = ${id}`)
+    const [verificacion] = await conexion.query(`SELECT * FROM TASKS WHERE ID = ${id};`)
+    if(verificacion.length == 0){
+        res.send("la id ingresada no existe")
+    }else{const [ consulta ] = await conexion.query(`DELETE FROM TASKS where id = ${id}`)
     res.send("Se elimino una tarea correctamente")
+    }
 }
 module.exports = crtl
